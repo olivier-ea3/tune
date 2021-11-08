284fbe84613f125efed24da0afb3e60e87aea620
Olivier Maillot <olivier.maillot@earthcube.eu>
2021-11-08 12:15:05 +0100

    Packaging the lib

---
+++ setup.py
@@ -0,0 +1,16 @@
+from pathlib import Path
+
+from setuptools import find_namespace_packages, setup
+
+setup(
+    name="tune",
+    version="1.0.1",
+    description="Benchmarking HF",
+    install_requires=Path("requirements.txt").read_text().splitlines(),
+    packages=find_namespace_packages(include=["src.*"]),
+    include_package_data=False,
+    extras_require={"dev": Path("requirements-dev.txt").read_text().splitlines()},
+    entry_points={
+        "console_scripts": ["benchmark=launcher:main"],
+    },
+)
